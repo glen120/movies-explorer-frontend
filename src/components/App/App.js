1c2ad3e58f60d97757305c41d49a4f52cf725023
Sergey Kornilov <amadeusdekond@yandex.ru>
2023-08-20 16:50:20 +0300

    register готов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main/Main';
+import Register from '../Register/Register';
 import './App.css';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
+      <Route path='/signup' element={<Register />} />
     </Routes>
   );
 }
