5460baf6f239f8cdb3d9fe6596d638544c6e23e0
Sergey Kornilov <amadeusdekond@yandex.ru>
2023-08-26 01:58:20 +0300

    Profile готов

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main/Main';
 import Movies from '../Movies/Movies/Movies';
 import SavedMovies from '../Movies/SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 import Page404 from '../Page404/Page404';
@@ -14,6 +15,7 @@ export default function App() {
       <Route path='/' element={<Main />} />
       <Route path='/movies' element={<Movies />} />
       <Route path='/saved-movies' element={<SavedMovies />} />
+      <Route path='/profile' element={<Profile />} />
       <Route path='/signup' element={<Register />} />
       <Route path='/signin' element={<Login />} />
       <Route path='*' element={<Page404 />} />
